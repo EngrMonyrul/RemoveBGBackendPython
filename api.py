6ee580f118db319882e4e006ef7c6bb079c4d5cb
Monirul-Islam <engrmonirulislam513@gmail.com>
2025-07-20 22:54:24 +0600

    space.yaml

--- api.py
+++ api.py
@@ -23,7 +23,3 @@ async def remove_bg(file: UploadFile=File(...)):
 @app.get("/")
 def read_root():
     return {"message": "Background remover backend running succesfully."}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("api:app", host="0.0.0.0", port=port)
